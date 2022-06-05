2f949d5b88235f8f4ff07ed4d904af82f7994989
MLopez <programadormlopez@gmail.com>
2022-06-05 18:28:28 -0300

    segundo commit

--- Nike/views.py
+++ Nike/views.py
@@ -3,23 +3,23 @@ from Nike.models import indumentarias,calzados,accesorios
 # Create your views here.
 
 
-def producto_add(request):
-    producto_nuevo = indumentarias.objects.create(
-        codigo_barra = '123',
-        nombre = 'Remera',
-        precio = 10,
-        talle = 's',
-        color = 'rojo',
-        cod_estacion = 1
-        )
-    context = {'producto_nuevo':producto_nuevo}
-    return render(request,'index.html',context=context)
-
-
 #def producto_add(request):
-#    producto_nuevo = indumentarias.objects.all()
+#    producto_nuevo = indumentarias.objects.create(
+#        codigo_barra = '123',
+#        nombre = 'Remera',
+#        precio = 10,
+#        talle = 's',
+#        color = 'rojo',
+#        cod_estacion = 1
+#        )
 #    context = {'producto_nuevo':producto_nuevo}
 #    return render(request,'index.html',context=context)
+
+
+def producto_add(request):
+    producto_nuevo = indumentarias.objects.all()
+    context = {'producto_nuevo':producto_nuevo}
+    return render(request,'index.html',context=context)
 
 #def indumentaria(request):
 #    producto_nuevo = indumentarias.objects.all()
